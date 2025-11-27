08b052f8877c20d9d1ebcd11a8dd8b9fc6f8097b
AmirPorm <promgrameramir@gmail.com>
2025-11-27 19:55:20 +0330

    memberControler

---
+++ backend/api/Controllers/MemberController.cs
@@ -0,0 +1,20 @@
+using api.Extensions;
+using api.Helpers;
+using Microsoft.AspNetCore.Authorization;
+
+namespace api.Controllers;
+
+[Authorize]
+public class MemberController(IMemberRepository memberRepository) : BaseApiController
+{
+    [HttpGet("get-by-username/{userName}")]
+    public async Task<ActionResult<MemberDto?>> GetByUserName(string userName, CancellationToken cancellationToken)
+    {
+        MemberDto? memberDto = await memberRepository.GetByUserNameAsync(userName, cancellationToken);
+
+        if (memberDto is null)
+            return BadRequest("User not found");
+
+        return memberDto;
+    }
+}
