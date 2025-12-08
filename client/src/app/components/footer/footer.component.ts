26888c449535d5cbda354c93af9d6caf6f993f09
AmirPorm <promgrameramir@gmail.com>
2025-12-08 23:19:55 +0330

    feat(Footer): complete fototer components whit impoved structure and UI

--- client/src/app/components/footer/footer.component.ts
+++ client/src/app/components/footer/footer.component.ts
@@ -1,12 +1,20 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+import { NgForOf } from '@angular/common';
 
 @Component({
   selector: 'app-footer',
   standalone: true,
-  imports: [],
+  imports: [RouterLink, NgForOf],
   templateUrl: './footer.component.html',
-  styleUrl: './footer.component.scss'
+  styleUrl: './footer.component.scss',
 })
 export class FooterComponent {
   currentYear = new Date().getFullYear();
+
+  links = [
+    { label: 'Home', url: '/' },
+    { label: 'Chat', url: '/chat' },
+    { label: 'Explore', url: '/explore' },
+  ];
 }
