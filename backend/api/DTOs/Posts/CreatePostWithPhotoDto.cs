d4cddb7b52d8ce9c6d7e5952c5f27785077632d8
AmirPorm <promgrameramir@gmail.com>
2026-03-18 00:30:04 +0330

    del(Delet Photo $$ files):Fils Photo=>744

--- backend/api/DTOs/Posts/CreatePostWithPhotoDto.cs
+++
@@ -1,9 +0,0 @@
-using Microsoft.AspNetCore.Http;
-
-namespace API.DTOs;
-
-public class CreatePostWithPhotoDto
-{
-    public string Caption { get; set; } = string.Empty;
-    public IFormFile File { get; set; } = null!;
-}
--- backend/api/DTOs/Posts/UpdatePost.cs
+++
@@ -1,6 +0,0 @@
-namespace API.DTOs;
-
-public class UpdatePostDto
-{
-    public string Caption { get; set; } = string.Empty;
-}
