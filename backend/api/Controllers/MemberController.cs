fa44aa7671d7cda93adb36f4f40e727bf283480d
AmirPorm <promgrameramir@gmail.com>
2026-03-17 23:12:22 +0330

    refactor(MemberController):  chnage Member params

--- backend/api/Controllers/MemberController.cs
+++ backend/api/Controllers/MemberController.cs
@@ -9,14 +9,16 @@ namespace api.Controllers;
 public class MemberController(IMemberRepository memberRepository) : BaseApiController
 {
     [HttpGet("get-all")]
-    public async Task<ActionResult<IEnumerable<MemberDto>>> GetAll([FromQuery] PaginationParams paginationParams, CancellationToken cancellationToken)
+    public async Task<ActionResult<IEnumerable<MemberDto>>> GetAll([FromQuery] MemberParams memberParams, CancellationToken cancellationToken)
     {
         var userId = User.GetUserId();
+
+        memberParams.UserId = userId;
 
         if (userId is null)
             return Unauthorized("You are not login. Please login again");
 
-        PagedList<AppUser> pagedAppUsers = await memberRepository.GetAllAsync(paginationParams, cancellationToken);
+        PagedList<AppUser> pagedAppUsers = await memberRepository.GetAllAsync(memberParams, cancellationToken);
 
         if (pagedAppUsers.Count == 0)
             return NoContent();
@@ -27,7 +29,7 @@ public class MemberController(IMemberRepository memberRepository) : BaseApiContr
         PaginationHeader paginationHeader = new(
             CurrentPage: pagedAppUsers.CurrentPage,
             ItemsPerPage: pagedAppUsers.PageSize,
-            TotalItems: pagedAppUsers.TotalItems,
+            TotalItemsCount: pagedAppUsers.TotalItemsCount,
             TotalPages: pagedAppUsers.TotalPages
         );
 
