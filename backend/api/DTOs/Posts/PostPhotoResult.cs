34e996b97c02a5bb4c41f198b8d18c34f37a5fdf
AmirPorm <promgrameramir@gmail.com>
2026-03-17 23:24:19 +0330

    feat(PostPhotoResult):

---
+++ backend/api/DTOs/Posts/PostPhotoResult.cs
@@ -0,0 +1,7 @@
+public class PostPhotoResult
+{
+    public string OriginalUrl { get; set; }
+    public string LargeUrl { get; set; }
+    public string MediumUrl { get; set; }
+    public string ThumbUrl { get; set; }
+}
